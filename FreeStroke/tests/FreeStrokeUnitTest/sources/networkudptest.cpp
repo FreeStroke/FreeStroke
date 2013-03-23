5a7a84e184b86f0a0a6d9a8e016ea5f5354e3d8d
Sylvain FAY-CHATELARD <sylvain.faychatelard@gmail.com>
2013-03-23 16:00:50 +0100

    Add network unit test

---
+++ FreeStroke/tests/FreeStrokeUnitTest/sources/networkudptest.cpp
@@ -0,0 +1,44 @@
+/*
+* Projet de fin d'études LastProject de
+* Adrien Broussolle
+* Camille Darcy
+* Guillaume Demurger
+* Sylvain Fay-Chatelard
+* Anthony Fourneau
+* Aurèle Lenfant
+* Adrien Madouasse
+*
+* Copyright (C) 2013 Université Paris-Est Marne-la-Vallée
+*
+* FreeStroke is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation; either
+* version 2.1 of the License, or (at your option) any later version.
+*
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public
+* License along with this library; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
+*/
+#include "../includes/networkudptest.h"
+
+NetowkrUdpTest::NetowkrUdpTest(std::string name) : CppUnit::TestCase(name) {}
+
+void NetowkrUdpTest::runTest()
+{
+    string name = "test";
+/*    atomic_bool atrue; atrue.store(true);
+    atomic_bool afalse; afalse.store(true);
+    atomic_ushort tport; tport.store(4242);
+    atomic_ushort uport1; uport1.store(4000);
+    atomic_ushort uport2; uport2.store(4001);
+    atomic_ushort uport3; uport3.store(4002);
+    atomic_ushort uport4; uport4.store(4003);
+    //UdpServer * srv = new UdpServer(&atrue, &afalse, &name, &tport, &uport1);
+    CPPUNIT_ASSERT( 1 == 1);
+    CPPUNIT_ASSERT( !(1 == 2));*/
+}
